304c58e96c11ec0cf9fe7566829a938803b75578
Ekalape <ekalape@outlook.it>
2023-06-11 09:10:29 +0200

    feat: create.js completed

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+/* const fs = require('fs');
+const path = require('path'); */
+import fs from 'fs';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const TEXT = 'I am fresh and young';
+
+const currentPath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+  fs.open(currentPath, 'wx', (err) => {
+    if (err) throw Error('FS operation failed');
+    else
+      fs.writeFile(currentPath, TEXT, (err) => {
+        if (err) throw Error('Writing operation failed');
+      });
+  });
 };
 
 await create();
