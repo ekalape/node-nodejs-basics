6f5981dd613b100f1e2b9832bbfbee4813c97cd6
Ekalape <ekalape@outlook.it>
2023-06-14 16:07:32 +0200

    feat: compress/decompress tasks are completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import * as url from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'node:stream';
+import { createGzip } from 'node:zlib';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const currentPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const archivePath = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  // compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+
+  const rStream = createReadStream(currentPath);
+  const wStream = createWriteStream(archivePath);
+  const compressFile = createGzip();
+  pipeline(rStream, compressFile, wStream, (err) => {
+    if (err) console.error('Error', err);
+    process.exit();
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import * as url from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'node:stream';
+import { createGunzip } from 'node:zlib';
+import { exit } from 'node:process';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const currentPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const archivePath = path.join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+  // decompresses archive.gz back to the fileToCompress.txt with same content as before compression using zlib and Streams API
+  const rStream = createReadStream(archivePath);
+  const wStream = createWriteStream(currentPath);
+  const compressFile = createGunzip();
+  pipeline(rStream, compressFile, wStream, (err) => {
+    if (err) console.error('Error', err);
+    exit();
+  });
 };
 
 await decompress();
